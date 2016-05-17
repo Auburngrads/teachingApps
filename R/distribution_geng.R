fa37cec23875119527d8958cf93e3c9251c363d2
Auburngrads <Auburngrads@live.com>
2016-05-16 22:40:18 -0400

    Chapter 5 apps

---
+++ R/distribution_geng.R
@@ -0,0 +1,100 @@
+distribution_geng <-
+function(...) {
+  
+  try(attachNamespace('shiny'), silent = TRUE)
+  try(attachNamespace('SMRD'), silent = TRUE)
+  try(attachNamespace('metricsgraphics'), silent = TRUE)
+  
+shinyApp(options = list(height = "700px"),
+ui = fluidPage(theme = shinythemes::shinytheme("flatly"),
+               try(includeCSS(system.file('css',
+                                          'my-shiny.css',
+                                          package = 'teachingApps')), silent = TRUE),
+sidebarLayout(
+sidebarPanel(width = 3,
+
+  sliderInput("range.geng", 
+              label = h2("Range"),
+              min =  0, 
+              max = 50, 
+              value = c(0,25)),
+  sliderInput("theta.geng", 
+              label = h2(HTML("&theta;")),
+              min = 0.5, 
+              max = 10, 
+              step = 0.5, 
+              value = 1,
+              animate = TRUE),
+  sliderInput("beta.geng", 
+              label = h2(HTML("&beta;")),
+              min = 0.5, 
+              max = 10, 
+              step = 0.5, 
+              value = 1, 
+              animate = TRUE),
+  sliderInput("kappa.geng",
+              label = h2(HTML("&kappa;")),
+              min = 0.5, 
+              max = 10, 
+              step = 0.5, 
+              value = 1,
+              animate = TRUE)),
+  mainPanel(width = 9,
+ tabsetPanel(type = 'pills',
+  tabPanel(h4('Distribution Function'),metricsgraphicsOutput("gengC",height = "600px")),
+  tabPanel(h4('Density'),              metricsgraphicsOutput("gengP",height = "600px")),
+  tabPanel(h4('Survival'),             metricsgraphicsOutput("gengR",height = "600px")),
+  tabPanel(h4('Hazard'),               metricsgraphicsOutput("gengh",height = "600px")),
+  tabPanel(h4('Cumulative Hazard'),    metricsgraphicsOutput("gengH",height = "600px")),
+  tabPanel(h4('Quantile'),             metricsgraphicsOutput("gengQ",height = "600px"))
+  )))),
+
+server = function(input, output, session) {
+  
+t <- reactive({ signif(seq(min(input$range.geng), max(input$range.geng), length = 500), digits = 4)})
+p <- signif(seq(0, 1, length = 500), digits = 4) 
+C <- reactive({ pgeng(t(), input$theta.geng, input$beta.geng, input$kappa.geng)})
+P <- reactive({ dgeng(t(), input$theta.geng, input$beta.geng, input$kappa.geng)})
+R <- reactive({ 1-C()})
+h <- reactive({ exp(log(P())/log(R()))})
+H <- reactive({ -1*log(1-pgeng(t(), input$theta.geng, input$beta.geng, input$kappa.geng))})
+Q <- reactive({ qgeng(p, input$theta.geng, input$beta.geng, input$kappa.geng)})
+df <- reactive({data.frame(Time = t(),PROB = p, CDF = C(),PDF = P(),REL = R(),haz = h(),HAZ = H(), QUANT = Q())})
+
+  output$gengC <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = CDF, decimals = 4, top = 0) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'F(t)')%>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")}) 
+  
+  output$gengP <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = PDF, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'f(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$gengR <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = REL, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'S(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$gengh <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = haz, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'h(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$gengH <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = HAZ, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'H(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$gengQ <- renderMetricsgraphics({
+  mjs_plot(df(), x = PROB, y = QUANT, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Probability', y_label = 'q(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+})
+}
---
+++ R/distribution_igau.R
@@ -0,0 +1,97 @@
+distribution_igau <-
+function(...) {
+  
+  try(attachNamespace('shiny'), silent = TRUE)
+  try(attachNamespace('SMRD'), silent = TRUE)
+  try(attachNamespace('metricsgraphics'), silent = TRUE)
+  
+shinyApp(options = list(height = "700px"),
+         
+ui = fluidPage(theme = shinythemes::shinytheme("flatly"),
+               try(includeCSS(system.file('css',
+                                          'my-shiny.css',
+                                          package = 'teachingApps')), silent = TRUE),
+sidebarLayout(
+sidebarPanel(width = 3, hr(),
+  
+  sliderInput("rangeigau", 
+              label = h2("Range"),
+              min =  0, 
+              max = 50, 
+              value = c(0,25)),
+  hr(),
+  sliderInput("th.igau", 
+              label = h2(HTML("Scale (&theta;)")),
+              min = .5, 
+              max = 10, 
+              step = .5, 
+              value = 1, 
+              animate = TRUE),
+  hr(),
+  sliderInput("be.igau", 
+              label = h2(HTML("Shape (&beta;)")),
+              min = .5, 
+              max = 10, 
+              step = .5, 
+              value = 1,
+              animate = TRUE)),
+
+mainPanel(width = 9,
+ tabsetPanel(type = 'pills',
+  tabPanel(h4('Distribution Function'),metricsgraphicsOutput("igauC",height = "600px")),
+  tabPanel(h4('Density'),              metricsgraphicsOutput("igauP",height = "600px")),
+  tabPanel(h4('Survival'),             metricsgraphicsOutput("igauR",height = "600px")),
+  tabPanel(h4('Hazard'),               metricsgraphicsOutput("igauh",height = "600px")),
+  tabPanel(h4('Cumulative Hazard'),    metricsgraphicsOutput("igauH",height = "600px")),
+  tabPanel(h4('Quantile'),             metricsgraphicsOutput("igauQ",height = "600px"))
+  )))),
+
+server = function(input, output, session) { 
+  
+t <-  reactive({ signif(seq(input$rangeigau[1], input$rangeigau[2], length = 500), dig = 4) })
+p <-  reactive({ signif(seq(0.001, .999, length = 500), digits = 4) })
+C <-  reactive({ pigau(t(), input$be.igau,input$th.igau)})
+P <-  reactive({ digau(t(), input$be.igau,input$th.igau)})
+R <-  reactive({ 1-C()})
+h <-  reactive({ exp(log(P())-log(R()))})
+H <-  reactive({ -1*log(1-pigau(t(), input$be.igau,input$th.igau))})
+Q <-  reactive({ qigau(p(), input$be.igau,input$th.igau) })
+df <- reactive({ data.frame(Time = t(),PROB = p(), CDF = C(),PDF = P(),REL = R(),haz = h(),HAZ = H(), QUANT = Q()) })
+
+  output$igauC <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = CDF, decimals = 4, top = 0) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'F(t)')%>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")}) 
+  
+  output$igauP <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = PDF, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'f(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$igauR <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = REL, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'S(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$igauh <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = haz, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'h(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$igauH <- renderMetricsgraphics({
+  mjs_plot(df(), x = Time, y = HAZ, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Time (t)', y_label = 'H(t)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+  
+  output$igauQ <- renderMetricsgraphics({
+  mjs_plot(df(), x = PROB, y = QUANT, decimals = 4) %>%
+  mjs_line(area = TRUE) %>%
+  mjs_labs(x_label = 'Probability (p)', y_label = 't(p)') %>%
+  mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
+})
+}
