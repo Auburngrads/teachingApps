8e1321896252bd17db803c381fee7dbcec4bbb73
Auburngrads <Auburngrads@live.com>
2016-05-18 23:10:39 -0400

    100 Apps!

--- R/zzz.R
+++ R/zzz.R
@@ -19,6 +19,23 @@ jkf.par <<- function(...) {
         las=1,
         tck=0.015,
         family='serif',...)
-    
 }
+}
+
+#' Leave signature for teachingApps 
+#'
+#' @param name The name of the app \code{character}
+#'
+#' @export
+
+teachingApp <- function(name) {
+  
+  if(!is.character(name)) stop('name must be a character string')
+  
+  gitFile <- paste(c(name,'.R'), collapse = '')
+  gitURL  <- 'https://github.com/Auburngrads/teachingApps/blob/master/R'
+  
+    return(paste(c("<div style='font-size: 22pt; font-weight: bold;'>Get the <a href='",
+                paste(c(gitURL, gitFile), collapse = '/'),"'>CODE</a> for this app</div>"), collapse = ''))
+    
 }
