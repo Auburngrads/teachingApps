ce4dbc00ef9bb621ca9f80a7209c0a23878a3d8b
Auburngrads <Auburngrads@live.com>
2016-06-11 18:57:16 -0400

    my-shiny.css

---
+++ R/offbase_libraries.R
@@ -0,0 +1,52 @@
+offbase_libraries <- function() {
+
+try(attachNamespace('shiny'), silent = TRUE)
+try(attachNamespace('leaflet'), silent = TRUE)
+try(attachNamespace('publicLibs'), silent = TRUE)
+try(attachNamespace('data.table'), silent = TRUE)
+
+shinyApp(options = list(height = '800px', width = '100%'),
+
+    ui = navbarPage(theme = shinythemes::shinytheme('flatly'),
+                    try(includeCSS(system.file('css',
+                                               'my-shiny.css',
+                                               package = 'teachingApps')), silent = T),
+
+tabPanel(h4('Off-Base Libraries'),
+         sidebarLayout(
+           sidebarPanel(width = 3,
+              selectInput('bases',
+                          h2('U.S. Air Force Bases'),
+                          choices = Base_Locations[,1],
+                          selected = 'Wright-Patterson AFB'),
+              sliderInput('radius',
+                          h2('Distance From Base'),
+                          min = 10,
+                          max = 100,
+                          step = 5,
+                          value = 20,
+                          animate = T)),
+           mainPanel(leafletOutput('libmap', height = '600px'), width = 9))),
+
+tabPanel(h4('About This Tool'),
+         mainPanel(uiOutput('aboutlib', height = '600px'), width = 12))),
+
+
+server = function(input, output, session) {
+
+output$libmap <- renderLeaflet({
+
+    abb <- Base_Locations[which(Base_Locations[,1]==input$bases),3]
+    lib <- eval(parse(text = paste(c(abb,'_Libs'), collapse = '')))
+    lib <- subset(lib, lib[,miles<=input$radius])
+
+
+    leaflet(lib) %>%
+      addTiles() %>%
+      addMarkers(~latitude,~longitude, popup = ~location)
+})
+output$aboutlib <- renderUI({HTML('good stuff goes here')})
+
+})
+}
+
