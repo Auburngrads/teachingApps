ccf54f95ec9e34fa70d4629dd946c54e34a76997
Auburngrads <Auburngrads@live.com>
2016-06-15 16:22:52 -0400

    again

--- R/offbase_libraries.R
+++ R/offbase_libraries.R
@@ -41,7 +41,7 @@ output$libmap <- renderLeaflet({
                              header = TRUE, stringsAsFactors = F)
     abb <- Base_Locs[which(Base_Locs[,1]%in%input$bases),3]
     lib <- eval(parse(text = paste(c(abb,'_Libs'), collapse = '')))
-    lib <- subset(lib, lib[,`miles`<=input$radius])
+    lib <- subset(lib, `miles`<=input$radius)
 
 
     leaflet(lib) %>%
