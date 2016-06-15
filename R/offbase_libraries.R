fa2297db26073320f4fc6395266657f53114886c
Auburngrads <Auburngrads@live.com>
2016-06-15 15:57:08 -0400

    crossing fingers

--- R/offbase_libraries.R
+++ R/offbase_libraries.R
@@ -36,8 +36,10 @@ server = function(input, output, session) {
 
 output$libmap <- renderLeaflet({
 
-    Base_Locs <- data.table(publicLibs::Base_Locations)
-    abb <- Base_Locs$`Base (Abbreviation)`[which(Base_Locs$`Base (Long Name)`==input$bases)]
+    Base_Locs <- read.table(system.file('extdata','Base_Locations.txt',
+                                         package = 'publicLibs'),
+                             header = TRUE, stringsAsFactors = F)
+    abb <- Base_Locs[which(Base_Locs[,1]%in%input$bases),3]
     lib <- eval(parse(text = paste(c(abb,'_Libs'), collapse = '')))
     lib <- subset(lib, lib[,`miles`<=input$radius])
 
