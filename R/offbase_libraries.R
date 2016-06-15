14a42c81ac9a8125f60f9ef47ea5033dd5fc499f
Auburngrads <Auburngrads@live.com>
2016-06-15 13:37:57 -0400

    WTF

--- R/offbase_libraries.R
+++ R/offbase_libraries.R
@@ -36,7 +36,7 @@ server = function(input, output, session) {
 
 output$libmap <- renderLeaflet({
 
-    Base_Locs <- data.table(Base_Locations)
+    Base_Locs <- data.table(publicLibs::Base_Locations)
     abb <- Base_Locs[which(Base_Locs[,`Base (Long Name)`==input$bases]),`Base (Abbreviation)`]
     lib <- eval(parse(text = paste(c(abb,'_Libs'), collapse = '')))
     lib <- subset(lib, lib[,`miles`<=input$radius])
