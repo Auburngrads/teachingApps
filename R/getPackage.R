4629c34e82564305e749705147bc93cb607e4f01
Auburngrads <Auburngrads@live.com>
2016-09-07 16:15:09 -0400

    getPackage done?

--- R/getPackage.R
+++ R/getPackage.R
@@ -3,15 +3,24 @@
 #' @export
 #' 
 #' @param pkg Name of a package to be installed/loaded (as a character string)
-#' @param repo The repository from which the package should be installed. 
+#' @param repo The repository from which the package should be installed.
+#' @param pub \code{logical} variable indicating whether the app be published (see details) 
 #' 
 #' @importFrom devtools install_github
+#' @importFrom utils install.packages
+#' @importFrom utils installed.packages
 #'
-#' @details If \code{repo = NULL} the package will be installed from the CRAN.  Otherwise, \code{repo} is a character string that referring to the GitHub account in which the package is located  
+#' @details If \code{repo = NULL} the package will be installed from the CRAN.  Otherwise, \code{repo} is a character string that referring to the GitHub account in which the package is located.
+#' 
+#'   When publishing apps on shinyapps.io or shinyServer, attempting to \code{install.packages} will result in an error.  Calls to \code{install.packages} should not be included within an app.
 
-getPackage <- function(pkg = NULL, repo = NULL, pub = FALSE ) {
+getPackage <- function(pkg = NULL, repo = NULL, pub = TRUE ) {
 
   if(pub) {
+    
+    do.call(library, list(package = pkg, character.only = TRUE))
+    
+  } else {
   
   if(is.null(repo)) {
 
@@ -19,10 +28,8 @@ if(!pkg%in%installed.packages()) install.packages(pkg, repos = 'http://cran.rstu
   
   } else {
 
-if(!pkg%in%installed.packages()) devtools::install_github(paste(c(b,a), collapse = '/')) 
+if(!pkg%in%installed.packages()) devtools::install_github(paste(c(repo,pkg), collapse = '/')) 
   
   }
   }
-do.call(library, list(package = pkg, character.only = TRUE))
-
   }
