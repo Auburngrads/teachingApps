9b64160b7b02b51dd55f73cd7f77e52d239b5cfa
Auburngrads <Auburngrads@live.com>
2016-09-19 12:36:55 -0400

    test eval5

--- R/getPackage.R
+++ R/getPackage.R
@@ -28,5 +28,5 @@ if(!pkg%in%installed.packages()) devtools::install_github(paste(c(repo,pkg), col
   
   }
   }
-  do.call(library, list(package = pkg, character.only = TRUE), envir = sys.frame())
+  do.call(loadNamespace, list(package = pkg), envir = sys.frame())
   }
