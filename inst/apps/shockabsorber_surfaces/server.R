2d2c592fb7c15454bd08dab6b1517246d26958a9
Auburngrads <auburngrads@live.com>
2017-06-11 15:16:04 -0400

    shockabsorber_surfaces

--- inst/apps/shockabsorber_surfaces/server.R
+++ inst/apps/shockabsorber_surfaces/server.R
@@ -1,5 +1,7 @@
 server = function(input, output, session) {
 
+  session$onFlush(function() teachingApps:::add_css())
+  
   output$shurf <- renderPlot({
       input$shocksurfs
       return(isolate(eval(parse(text=input$shocksurf))))
--- inst/apps/shockabsorber_surfaces/ui.R
+++ inst/apps/shockabsorber_surfaces/ui.R
@@ -1,13 +1,10 @@
 library(teachingApps)
 library('SMRD')
 
-ui = fluidPage(theme = shinythemes::shinytheme(theme = getShinyOption("theme")), 
-               tags$head(includeCSS(getShinyOption("CSS"))),
-               tags$head(includeCSS("www/custom.css")),
-               tags$footer(getShinyOption("sign")),
+ui = fluidPage(theme = teachingApps::add_theme(theme = getShinyOption("theme")), 
                
 sidebarLayout(
-   sidebarPanel(width = 4,
+   sidebarPanel(width = 5,
       shinyAce::aceEditor(fontSize = 16, 
                           wordWrap = T,
                           outputId = "shocksurf", 
@@ -15,31 +12,34 @@ sidebarLayout(
                           theme = "github", 
                           height = "450px", 
                           value = "
-par(family = 'serif', mfrow = c(1,2), las = 1, cex = 1.25)
+par(mfrow = c(1,2), las = 1)
 
 library(SMRD)
 
 zoom <- 1.5
+threeD <- F
 
-ShockAbsorber.ld <- 
-frame.to.ld(SMRD::shockabsorber,
+Shock.ld <- 
+frame.to.ld(shockabsorber,
             response.column = 1, 
             censor.column = 3,
             time.units = 'Kilometers')
 
-simple.contour(ShockAbsorber.ld, 
+simple.contour(Shock.ld, 
                distribution = 'weibull', 
-               rel.or.conf = 'Joint confidence region', 
-               factor = zoom,
-               original.par = F)
+               show.confidence = T, 
+               zoom.level = zoom,
+               original.par = F,
+               threeD = threeD)
 
-simple.contour(ShockAbsorber.ld, 
+simple.contour(Shock.ld, 
                distribution = 'weibull', 
-               rel.or.conf = 'Relative likelihood', 
-               factor = zoom)
+               show.confidence = F, 
+               zoom.level = zoom,
+               threeD = threeD)
 
 par(mfrow = c(1,1))"),
              
         actionButton("shocksurfs", h4("Evaluate"), width = '100%')),
         
-        mainPanel(plotOutput("shurf", height = "600px"), width = 8)))
+        mainPanel(plotOutput("shurf", height = "600px"), width = 7)))
