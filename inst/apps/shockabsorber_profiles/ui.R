0d48cb27777ae6cf782204649a71cf2ac38f2771
Auburngrads <auburngrads@live.com>
2017-06-11 16:17:26 -0400

    shockabsorber_profiles

--- inst/apps/shockabsorber_profiles/ui.R
+++ inst/apps/shockabsorber_profiles/ui.R
@@ -3,8 +3,8 @@ library('SMRD')
 
 ui = fluidPage(theme = shinythemes::shinytheme(theme = getShinyOption("theme")), 
                tags$head(includeCSS(getShinyOption("CSS"))),
-               tags$head(includeCSS("www/custom.css")),
-               tags$footer(getShinyOption("sign")),
+               #tags$head(includeCSS("www/custom.css")),
+               #tags$footer(getShinyOption("sign")),
 
 sidebarLayout(
    sidebarPanel(width = 5,
