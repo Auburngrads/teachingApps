403d8bbbc897c73f69da090ddb038a2133c68f63
Auburngrads <auburngrads@live.com>
2017-07-02 01:44:27 -0400

    update test app

--- inst/apps/test_app/server.R
+++ inst/apps/test_app/server.R
@@ -10,48 +10,10 @@ server = function(input, output, session) {
    })
  }
 
-lineChartOutput <- function(inputId, width="100%", height="400px") {
-   style <- sprintf("width: %s; height: %s;",
-     validateCssUnit(width), validateCssUnit(height))
 
-   tagList(
-     # Include CSS/JS dependencies. Use "singleton" to make sure that even
-     # if multiple lineChartOutputs are used in the same page, we'll still
-     # only include these chunks once.
-     singleton(tags$head(
-       tags$script(src="d3/d3.v3.min.js"),
-       tags$script(src="nvd3/nv.d3.min.js"),
-       tags$link(rel="stylesheet", type="text/css", href="nvd3/nv.d3.min.css"),
-       tags$script(src="linechart-binding.js")
-     )),
-     div(id=inputId, class="nvd3-linechart", style=style,
-       tag("svg", list())
-     )
-   )
- }
 
  # To be called from server.R
- renderLineChart <- function(expr, env=parent.frame(), quoted=FALSE) {
-   # This piece of boilerplate converts the expression `expr` into a
-   # function called `func`. It's needed for the RStudio IDE's built-in
-   # debugger to work properly on the expression.
-   installExprFunction(expr, "func", env, quoted)
-
-   function() {
-     dataframe <- func()
-
-      mapply(function(col, name) {
-
-       values <- mapply(function(val, i) {
-         list(x = i, y = val)
-       }, col, 1:nrow(dataframe), SIMPLIFY=FALSE, USE.NAMES=FALSE)
-
-
-       list(key = name, values = values)
-
-     }, dataframe, names(dataframe), SIMPLIFY=FALSE, USE.NAMES=FALSE)
-   }
- }
+ 
 
 
  # Data frame or list looks like:
@@ -73,7 +35,7 @@ lineChartOutput <- function(inputId, width="100%", height="400px") {
  #     values: [{x:1,y:6}, {x:2,y:7}, {x:3,y:8}, {x:4,y:9}, {x:5,y:10}]
  #   }
  # ]
- dataFrameToD3 <- function(df=cars) {
-
-
-}
+#  dataFrameToD3 <- function(df=cars) {
+# 
+# 
+# }
