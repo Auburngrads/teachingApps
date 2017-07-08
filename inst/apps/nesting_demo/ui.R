f87a80836dc6e39351af15147ac9e58277015528
Auburngrads <auburngrads@live.com>
2017-07-08 10:31:26 -0400

    update nesting_demo

--- inst/apps/nesting_demo/ui.R
+++ inst/apps/nesting_demo/ui.R
@@ -6,26 +6,19 @@ ui = navbarPage(title = 'Armadillo Hunting',
                 footer = add_logo(),
 
 
-tabPanel('Example 01',
-         add_ui(path = dirs[1])),
+tabPanel('Example 01', add_ui(path = dirs[1])),
 
-tabPanel('Example 02',
-         add_ui(path = dirs[2])),
+tabPanel('Example 02', add_ui(path = dirs[2])),
 
-tabPanel('Example 04',
-         add_ui(path = dirs[4])),
+tabPanel('Example 04', add_ui(path = dirs[4])),
 
-tabPanel('Example 05',
-         add_ui(path = dirs[5])),
+tabPanel('Example 05', add_ui(path = dirs[5])),
 
-tabPanel('Example 09',
-         add_ui(path = dirs[9])),
+tabPanel('Example 09', add_ui(path = dirs[9])),
 
-tabPanel('Example 10',
-         add_ui(path = dirs[10])),
+tabPanel('Example 10', add_ui(path = dirs[10])),
 
-tabPanel('Example 11',
-         add_ui(path = dirs[11]))
+tabPanel('Example 11', add_ui(path = dirs[11]))
 )
 
 
