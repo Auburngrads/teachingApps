b003d23109a0ce6e299a37385f8e0d4c718646ef
whaleshark16 <emilymeyer19@gmail.com>
2018-02-06 10:42:15 -0500

    Fixed server & ui scripts to reference hypergeometric functions instead of geometric. Also added in x-axis range in ui file to enable plotting.

--- inst/apps/distribution_hypergeometric_functions/server.R
+++ inst/apps/distribution_hypergeometric_functions/server.R
@@ -1,13 +1,13 @@
 server = function(input, output, session) {
 
-  t <- reactive({ input$N.hyp })
+  t <- reactive({ min(input$range.hyp):max(input$range.hyp) })
   p <- reactive({ signif(seq(0, 1, length = length(max(t()):min(t()))), digits = 4) })
-  C <- reactive({ pgeom(t(), input$prob.geom)})
-  P <- reactive({ dgeom(t(), input$prob.geom)})
+  C <- reactive({ phyper(t(), input$m.hyp, input$n.hyp, input$k.hyp)})
+  P <- reactive({ dhyper(t(), input$m.hyp, input$n.hyp, input$k.hyp)})
   R <- reactive({ 1 - C()})
   h <- reactive({ exp(log(P())-log(R()))})
-  H <- reactive({ -1 * log(1 - pgeom(t(),input$prob.geom))})
-  Q <- reactive({ qgeom(p(),input$prob.geom)})
+  H <- reactive({ -1 * log(1 - phyper(t(),input$m.hyp, input$n.hyp, input$k.hyp))})
+  Q <- reactive({ qhyper(p(),input$m.hyp, input$n.hyp, input$k.hyp)})
  df <- reactive({ data.frame(Time = t(), 
                              PROB = p(), 
                              CDF = C(), 
@@ -18,37 +18,37 @@ server = function(input, output, session) {
                              QUANT = Q()
                              )})
  
-      output$geomC <- renderMetricsgraphics({
+      output$hyperC <- renderMetricsgraphics({
         mjs_plot(df(), x = Time, y = CDF, decimals = 4, top = 0) %>% 
           mjs_point(point_size = 10)                              %>% 
           mjs_labs(x_label = "X = x", y_label = "F(x)")       %>% 
           mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")
       })
-      output$geomP <- renderMetricsgraphics({
+      output$hyperP <- renderMetricsgraphics({
         mjs_plot(df(), x = Time, y = PMF, decimals = 4)         %>% 
           mjs_point(point_size = 10)                             %>%
           mjs_labs(x_label = "X = x", y_label = "f(x)")      %>% 
           mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")
       })
-      output$geomR <- renderMetricsgraphics({
+      output$hyperR <- renderMetricsgraphics({
         mjs_plot(df(), x = Time, y = REL, decimals = 4)         %>% 
           mjs_point(point_size = 10)                             %>% 
           mjs_labs(x_label = "X = x", y_label = "S(x)")      %>% 
           mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")
       })
-      output$geomh <- renderMetricsgraphics({
+      output$hyperh <- renderMetricsgraphics({
         mjs_plot(df(), x = Time, y = haz, decimals = 4) %>%
           mjs_point(point_size = 10)  %>%
           mjs_labs(x_label = "X = x", y_label = "h(x)") %>%
           mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")
       })
-      output$geomH <- renderMetricsgraphics({
+      output$hyperH <- renderMetricsgraphics({
         mjs_plot(df(), x = Time, y = HAZ, decimals = 4) %>% 
           mjs_point(point_size = 10)  %>% 
           mjs_labs(x_label = "X = x", y_label = "H(x)") %>% 
           mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")
       })
-      output$geomQ <- renderMetricsgraphics({
+      output$hyperQ <- renderMetricsgraphics({
         mjs_plot(df(), x = PROB, y = QUANT, decimals = 4) %>%
           mjs_point(point_size = 10)  %>%
           mjs_labs(x_label = "Probability (p)", y_label = "x(p)") %>%
