8e8135a80b080cd6864d5b234df87ef78172af80
Auburngrads <auburngrads@live.com>
2018-06-15 16:33:12 -0400

    Update inst/apps/probability_functions/ui.R

--- inst/apps/probability_functions/ui.R
+++ inst/apps/probability_functions/ui.R
@@ -20,36 +20,11 @@ tabsetPanel(type = 'pills',
   tabPanel('Properties of the CDF',
            uiOutput('cdfdemo', class = 'ta-text')),
 
-  tabPanel('Computing CDF Values in R',
+  tabPanel('Computing Values in R',
            uiOutput('cdfr', class = 'ta-text')),
                                 
-  tabPanel('Interactive CDF Shiny App',
-    sidebarLayout( 
-      sidebarPanel(width = 4,
-        shinyAce::aceEditor(fontSize = 16, 
-                            wordWrap = T,
-                            outputId = "cdfplot", 
-                            mode = "r", 
-                            theme = "github", 
-                            value = 
-"par(family = 'serif',mar = c(4,6,2,1))
-
-curve(
-pweibull(x,shape = 1.7, scale = 1),
-xlab = 'Time, t',
-ylab = expression(F(t)[Weibull]),
-ylim = c(0,1),
-xlim = c(0,3),
-lwd = 3,
-lty = 1,
-col = 2,
-cex.lab = 1.5,
-cex.axis = 1.5,
-las = 1)"),
-
-        actionButton("evalcdf", "Evaluate")),
-        
-        mainPanel(plotOutput("plotcdf", height = "600px"), width = 8))))),
+  tabPanel('Interactive Shiny App',
+           add_ui('cdf_ace')))),
 
 tabPanel('Probability Density Function', 
         tabsetPanel(type = 'pills',
@@ -59,29 +34,55 @@ tabPanel('Probability Density Function',
 
   tabPanel('Computing Values in R',
            uiOutput('pdfr', class = 'ta-text')),
-                    
-  tabPanel('Shiny App',
-    sidebarLayout(
-      sidebarPanel(
-        shinyAce::aceEditor(fontSize = 16, 
-                            wordWrap = T,
-                            outputId = "pdfplot", 
-                            mode = "r", 
-                            theme = "github", 
-                            value = 
-"par(family = 'serif',mar = c(4,6,2,1))
+  
+  tabPanel('Interactive Shiny App',
+           add_ui('pdf_ace')))),
 
-curve(dexp(x,rate = 1.7),
-      xlab = 'Time, t',
-      ylab = expression(f(t)[Exponential]),
-      xlim = c(0,3),
-      lwd = 3,
-      lty = 2,
-      col = 3,
-      cex.lab = 1.5,
-      cex.axis = 1.5,
-      las = 1)"),
+tabPanel('Survival (Reliability) Function', 
+tabsetPanel(type = 'pills',
+                                
+  tabPanel('Properties of the Survival Function',
+           uiOutput('reldemo', class = 'ta-text')),
 
-        actionButton("evalpdf", "Evaluate")),
-        
-        mainPanel(plotOutput("plotpdf", height = "600px"))))))))
+  tabPanel('Computing Values in R',
+           uiOutput('relr', class = 'ta-text')),
+                                
+  tabPanel('Interactive Shiny App',
+           add_ui('rel_ace')))),
+
+tabPanel('Hazard Function', 
+tabsetPanel(type = 'pills',
+                                
+  tabPanel('Properties of the Hazard Function',
+           uiOutput('hazdemo', class = 'ta-text')),
+
+  tabPanel('Computing Values in R',
+           uiOutput('hazr', class = 'ta-text')),
+                                
+  tabPanel('Interactive Shiny App',
+           add_ui('haz_ace')))),
+
+tabPanel('Cumulative Hazard Function', 
+tabsetPanel(type = 'pills',
+                                
+  tabPanel('Properties of the Cumulative Hazard Function',
+           uiOutput('chazdemo', class = 'ta-text')),
+
+  tabPanel('Computing Values in R',
+           uiOutput('chazr', class = 'ta-text')),
+                                
+  tabPanel('Interactive Shiny App',
+           add_ui('chaz_ace')))),
+
+
+tabPanel('Quantile Function', 
+tabsetPanel(type = 'pills',
+                                
+  tabPanel('Properties of the Quantile Function',
+           uiOutput('quandemo', class = 'ta-text')),
+
+  tabPanel('Computing Values in R',
+           uiOutput('quanr', class = 'ta-text')),
+                                
+  tabPanel('Interactive Shiny App',
+           add_ui('quan_ace'))))))
