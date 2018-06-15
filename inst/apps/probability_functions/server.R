1e7b58d9139f5f066b5ba98302a99071d496df96
Auburngrads <auburngrads@live.com>
2018-06-15 16:33:09 -0400

    Update inst/apps/probability_functions/server.R

--- inst/apps/probability_functions/server.R
+++ inst/apps/probability_functions/server.R
@@ -1,35 +1,30 @@
 server = function(input, output, session) {
 
-output$overview <- renderUI({ 
-  withMathJax(HTML(includeMarkdown('overview.Rmd')))
-})
+output$overview <- renderUI({ add_rmd('overview.Rmd') })
 
-output$functable <- renderUI({ 
-  withMathJax(HTML(includeMarkdown('functable.Rmd')))
-}) 
+output$functable <- renderUI({ add_rmd('functable.Rmd') }) 
     
-output$cdfdemo <- renderUI({ 
-  withMathJax(HTML(includeMarkdown('backgroundcdf.Rmd')))
-})
+output$cdfdemo <- renderUI({ add_rmd('backgroundcdf.Rmd') })
+output$cdfr    <- renderUI({ add_rmd('rfuncscdf.Rmd') })  
+      add_server('cdf_ace', env = environment())
 
-output$cdfr <- renderUI({ 
-  withMathJax(HTML(includeMarkdown('rfuncscdf.Rmd')))
-})  
-
-output$plotcdf <- renderPlot({
-    input$evalcdf      
-    return(isolate(eval(parse(text=input$cdfplot))))
-})
-output$pdfdemo <- renderUI({ 
-  withMathJax(HTML(includeMarkdown('backgroundpdf.Rmd')))
-})
-
-output$pdfr <- renderUI({ 
-  withMathJax(HTML(includeMarkdown('rfuncspdf.Rmd')))
-})  
-
-output$plotpdf <- renderPlot({
-  input$evalpdf
-  return(isolate(eval(parse(text=input$pdfplot))))
-})
+output$pdfdemo <- renderUI({ add_rmd('backgroundpdf.Rmd') })
+output$pdfr    <- renderUI({ add_rmd('rfuncspdf.Rmd') })  
+      add_server('pdf_ace', env = environment())
+      
+output$reldemo <- renderUI({ add_rmd('backgroundsurv.Rmd') })
+output$relr    <- renderUI({ add_rmd('rfuncssurv.Rmd') })  
+      add_server('rel_ace', env = environment())
+      
+output$hazdemo <- renderUI({ add_rmd('backgroundhaz.Rmd') })
+output$hazr    <- renderUI({ add_rmd('rfuncshaz.Rmd') })  
+      add_server('haz_ace', env = environment())
+      
+output$chazdemo <- renderUI({ add_rmd('backgroundchaz.Rmd') })
+output$chazr    <- renderUI({ add_rmd('rfuncschaz.Rmd') })  
+      add_server('chaz_ace', env = environment())
+      
+output$quandemo <- renderUI({ add_rmd('backgroundquan.Rmd') })
+output$quanr    <- renderUI({ add_rmd('rfuncsquan.Rmd') })  
+      add_server('quan_ace', env = environment())
 }
