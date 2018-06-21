046d783b2faea490dc5f31a6aa8d63461e892dde
Auburngrads <auburngrads@live.com>
2018-06-21 11:40:56 -0400

    Update inst/apps/confidence_intervals_simulation/server.R

--- inst/apps/confidence_intervals_simulation/server.R
+++ inst/apps/confidence_intervals_simulation/server.R
@@ -1,12 +1,11 @@
 server = function(input, output, session) {
 
-shapeGamma <- 2
-scaleGamma <- 50
-xSkew <- seq(0,shapeGamma*scaleGamma+7.5*sqrt(shapeGamma)*scaleGamma,
-             length.out=600)
-ySkew <- dgamma(xSkew,shape=shapeGamma,scale=scaleGamma)
+meanlog <- 2
+sdlog   <- .50
+xSkew <- seq(0,35, length.out=600)
+ySkew <- dlnorm(xSkew,meanlog = meanlog,sdlog = sdlog)
 popDen <- list(x=xSkew,y=ySkew)
-popMean <- shapeGamma*scaleGamma
+popMean <- exp(meanlog)
 yMax <- 1.5*max(popDen$y)
   
 set.seed(as.numeric(Sys.time()))
@@ -17,19 +16,21 @@ rv <- reactiveValues(sample = NULL, mean = NULL, lower = NULL, upper = NULL, sim
 observeEvent(input$takeSample, 
                {
                  # random sample and its mean
-                 samp <- rgamma(input$n,shape=shapeGamma,scale=scaleGamma)
-                 xbar <-  mean(samp)
+                 samp <- rlnorm(input$n,meanlog = meanlog,sdlog = sdlog)
+                 fit <-  fitdistrplus::fitdist(samp, distr = 'lnorm')
+                 xbar <- exp(fit$estimate[1])
                  # make bounds for the confidence interval
                  conf = isolate(input$confLevel/100)
+                 alpha = 1 - conf
                  t.input = conf + ((1 - conf)/2)
                  tMultiplier = qt(t.input, df = input$n - 1)
-                 se = sd(samp)/sqrt(input$n)
+                 se = sqrt(fit$vcov[1,1]) * xbar # sd(samp)/sqrt(input$n)
                  margin = tMultiplier * se
                  # store in rv
-                 rv$sample <- rgamma(input$n,shape=shapeGamma,scale=scaleGamma)
+                 rv$sample <- samp
                  rv$mean <- xbar
-                 rv$lower <- xbar - margin
-                 rv$upper <- xbar + margin
+                 rv$lower <- xbar - qnorm(1-alpha/2) * se
+                 rv$upper <- xbar + qnorm(1-alpha/2) * se
                  goodInterval <- popMean >= rv$lower & popMean <= rv$upper
                  rv$sims <- rv$sims + 1
                  rv$good <- rv$good + goodInterval
@@ -50,8 +51,8 @@ par(family = "serif", font = 2, bg = NA)
          xlab="",
          ylab="",
          ylim = c(0,yMax))
-    text(c(400,500,600),rep(0.0108,3),c("Simulations","Good","Percent Good"), cex = 1.4)
-    text(c(400,500,600), rep(0.0100,3),c(rv$sims, rv$good, round(rv$good/rv$sims, digits = 4)), cex = 1.4)
+    text(c(20,25,30),rep(0.99*yMax,3),c("Simulations","Good","Percent Good"), cex = 1.4)
+    text(c(20,25,30), rep(0.95*yMax,3),c(rv$sims, rv$good, round(rv$good/rv$sims, digits = 4)), cex = 1.4)
     box(lwd = 2.5)
     axis(side = 2, las = 1, cex.axis = 1.25, tck = 0.025, hadj = 0.65)
     mtext(side = 2, "density f(t)", line = 3, cex.lab = 1.25)
@@ -66,7 +67,7 @@ par(family = "serif", font = 2, bg = NA)
       lastx <- xdens[length(xdens)]
       polygon(x = c(firstx,xdens,lastx), y = c(0,ydens,0), col = alpha("lightblue",0.5))
       # now the interval
-      intLevel <- 0.95*yMax
+      intLevel <- 0.9*yMax
       segments(x0 = rv$lower, y0 = intLevel, x1 = rv$upper, y1 = intLevel, 
                col = "green", lwd = 3)
       text(x=rv$lower,y=intLevel,labels="(")
