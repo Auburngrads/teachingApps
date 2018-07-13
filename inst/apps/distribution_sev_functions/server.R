e3c50e65265ba3eb51025dd6b9a8cc8a8b0ff1d2
Auburngrads <auburngrads@live.com>
2018-07-13 10:26:55 -0400

    Update inst/apps/distribution_sev_functions/server.R

--- inst/apps/distribution_sev_functions/server.R
+++ inst/apps/distribution_sev_functions/server.R
@@ -1,47 +1,54 @@
 server = function(input, output, session) {
   
-t = reactive({ signif(seq(min(input$range.s), max(input$range.s), length = 500), digits = 4)})
-p <- signif(seq(0, 1, length = 500), digits = 4) 
-C <- reactive({ psev(t(), input$mu.sev, input$sig.sev)})
-P <- reactive({ dsev(t(), input$mu.sev, input$sig.sev)})
-R <- reactive({ 1-C()})
-h <- reactive({ exp(log(P())-log(R()))})
-H <- reactive({ -1*log(1-psev(t(), input$mu.sev, input$sig.sev))})
-Q <- reactive({ qsev(p, input$mu.sev, input$sig.sev)})
-df <- reactive({data.frame(Time = t(),PROB = p, CDF = C(),PDF = P(),REL = R(),haz = h(),HAZ = H(), QUANT = Q())})
+sev.t = reactive({ signif(seq(min(input$range.s), max(input$range.s), length = 500), digits = 4)})
+sev.p <- signif(seq(0, 1, length = 500), digits = 4) 
+sev.C <- reactive({ psev(sev.t(), input$mu.sev, input$sig.sev)})
+sev.P <- reactive({ dsev(sev.t(), input$mu.sev, input$sig.sev)})
+sev.R <- reactive({ 1-sev.C()})
+sev.h <- reactive({ exp(log(sev.P())-log(sev.R()))})
+sev.H <- reactive({ -1*log(1-psev(sev.t(), input$mu.sev, input$sig.sev))})
+sev.Q <- reactive({ qsev(sev.p, input$mu.sev, input$sig.sev)})
+sev.df <- reactive({data.frame(Time  = sev.t(),
+                               PROB  = sev.p, 
+                               CDF   = sev.C(),
+                               PDF   = sev.P(),
+                               REL   = sev.R(),
+                               haz   = sev.h(),
+                               HAZ   = sev.H(), 
+                               QUANT = sev.Q())})
 
   output$sevC <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = CDF, decimals = 4, top = 0) %>%
+  mjs_plot(sev.df(), x = Time, y = CDF, decimals = 4, top = 0) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'F(t)')%>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")}) 
   
   output$sevP <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = PDF, decimals = 4) %>%
+  mjs_plot(sev.df(), x = Time, y = PDF, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'f(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$sevR <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = REL, decimals = 4) %>%
+  mjs_plot(sev.df(), x = Time, y = REL, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'S(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$sevh <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = haz, decimals = 4) %>%
+  mjs_plot(sev.df(), x = Time, y = haz, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'h(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$sevH <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = HAZ, decimals = 4) %>%
+  mjs_plot(sev.df(), x = Time, y = HAZ, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'H(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$sevQ <- renderMetricsgraphics({
-  mjs_plot(df(), x = PROB, y = QUANT, decimals = 4) %>%
+  mjs_plot(sev.df(), x = PROB, y = QUANT, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Probability (p)', y_label = 't(p)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
