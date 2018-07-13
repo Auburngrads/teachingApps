0948fdb003b7f2c8e6c78e40d042bf5c5f5c4a09
Auburngrads <auburngrads@live.com>
2018-07-13 10:26:49 -0400

    Update inst/apps/distribution_loglogistic_functions/server.R

--- inst/apps/distribution_loglogistic_functions/server.R
+++ inst/apps/distribution_loglogistic_functions/server.R
@@ -1,47 +1,54 @@
 server = function(input, output, session) {
   
-t = reactive({ signif(seq(min(input$range.ll), max(input$range.ll), length = 500), digits = 4)})
-p <- signif(seq(0, 1, length = 500), digits = 4) 
-C <- reactive({ pllogis(t(), input$mu.ll, input$sig.ll)})
-P <- reactive({ dllogis(t(), input$mu.ll, input$sig.ll)})
-R <- reactive({ 1-C()})
-h <- reactive({ exp(log(P())-log(R()))})
-H <- reactive({ -1*log(1-pllogis(t(), input$mu.ll, input$sig.ll))})
-Q <- reactive({ qllogis(p, input$mu.ll, input$sig.ll)})
-df <- reactive({data.frame(Time = t(),PROB = p, CDF = C(),PDF = P(),REL = R(),haz = h(),HAZ = H(), QUANT = Q())})
+llog.t = reactive({ signif(seq(min(input$range.ll), max(input$range.ll), length = 500), digits = 4)})
+llog.p <- signif(seq(0, 1, length = 500), digits = 4) 
+llog.C <- reactive({ pllogis(llog.t(), input$mu.ll, input$sig.ll)})
+llog.P <- reactive({ dllogis(llog.t(), input$mu.ll, input$sig.ll)})
+llog.R <- reactive({ 1-llog.C()})
+llog.h <- reactive({ exp(log(llog.P())-log(llog.R()))})
+llog.H <- reactive({ -1*log(1-pllogis(llog.t(), input$mu.ll, input$sig.ll))})
+llog.Q <- reactive({ qllogis(llog.p, input$mu.ll, input$sig.ll)})
+llog.df <- reactive({data.frame(Time  = llog.t(),
+                                PROB  = llog.p, 
+                                CDF   = llog.C(),
+                                PDF   = llog.P(),
+                                REL   = llog.R(),
+                                haz   = llog.h(),
+                                HAZ   = llog.H(),
+                                QUANT = llog.Q())})
 
   output$llogC <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = CDF, decimals = 4, top = 0) %>%
+  mjs_plot(llog.df(), x = Time, y = CDF, decimals = 4, top = 0) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'F(t)')%>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")}) 
   
   output$llogP <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = PDF, decimals = 4) %>%
+  mjs_plot(llog.df(), x = Time, y = PDF, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'f(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$llogR <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = REL, decimals = 4) %>%
+  mjs_plot(llog.df(), x = Time, y = REL, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'S(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$llogh <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = haz, decimals = 4) %>%
+  mjs_plot(llog.df(), x = Time, y = haz, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'h(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$llogH <- renderMetricsgraphics({
-  mjs_plot(df(), x = Time, y = HAZ, decimals = 4) %>%
+  mjs_plot(llog.df(), x = Time, y = HAZ, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Time (t)', y_label = 'H(t)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
   
   output$llogQ <- renderMetricsgraphics({
-  mjs_plot(df(), x = PROB, y = QUANT, decimals = 4) %>%
+  mjs_plot(llog.df(), x = PROB, y = QUANT, decimals = 4) %>%
   mjs_line(area = TRUE) %>%
   mjs_labs(x_label = 'Probability (p)', y_label = 't(p)') %>%
   mjs_add_css_rule("{{ID}} .mg-active-datapoint { font-size: 20pt }")})
